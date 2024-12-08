15d4b2ef9f3ca80c526c9b39804aa7aaadce61a1
Len Chang <lamgo.chang@gmail.com>
2024-12-09 00:34:24 +0800

    implement
    - add more ways to resolve the question
    - fine-tuning the code

--- completed_exercises/reversestring/index.js
+++ completed_exercises/reversestring/index.js
@@ -6,25 +6,59 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-  return str.split('').reduce((rev, char) => char + rev, '');
-}
+/**
+ * Pesudo Code
+ * @param {*} str
+ * @returns
+ * 1. Split the string to elements
+ * 2. (JS Func) Do reverse func
+ * 3. Combine elements into a string
+ */
+const reverse = (str) => str.split("").reverse().join("");
 
 module.exports = reverse;
 
-// function reverse(str) {
-//   return str
-//     .split('')
-//     .reverse()
-//     .join('');
-// }
-
-// function reverse(str) {
-//   let reversed = '';
-//
-//   for (let character of str) {
-//     reversed = character + reversed;
+/**
+ * Pesudo Code
+ * @param {*} str
+ * 1. Split the string into elements by character
+ * 2. Loop the elements (array) and excahnge the value between index 0 and index n-1, then do index 0+y and index n-1-y from y =0,1,2,...
+ * 3. End the loop until index y = index n-y
+ * 4. Combine elements into a string
+ */
+// const reverse = (str) => {
+//   const arrEle = str.split("");
+//   for (let i = 0; i + 1 <= arrEle.length / 2; i++) {
+//     let tmp = arrEle[i];
+//     arrEle[i] = arrEle[arrEle.length - 1 - i];
+//     arrEle[arrEle.length - 1 - i] = tmp;
 //   }
-//
+//   return arrEle.join("");
+// };
+
+/**
+ * Pesudo Code
+ * @param {*} str
+ * @returns
+ * 1. Split the string to elements
+ * 2. (JS Func) Create a tmp variable and push the element which is popped from the last looply
+ * 3. Combine elements into a string
+ */
+// const reverse = (str) => str.split("").reduce((acc, curr) => curr + acc, "");
+
+/**
+ * Pesudo Code
+ * @param {*} str
+ * @returns
+ * 1. Split the string to elements
+ * 2. Create a tmp variable and push the element which is popped from the last looply
+ * 3. Combine elements into a string
+ */
+// const reverse = (str) => {
+//   let reversed = "";
+//   for (let ele of str) {
+//     reversed = ele + reversed;
+//   }
+
 //   return reversed;
-// }
+// };
