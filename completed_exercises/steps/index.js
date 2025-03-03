43d6db9519ce87a22dd9206ee17cf89d613a5df8
Len Chang <lamgo.chang@gmail.com>
2025-03-03 15:50:23 +0800

    Update index.js

--- completed_exercises/steps/index.js
+++ completed_exercises/steps/index.js
@@ -33,18 +33,42 @@ function steps(n, row = 0, stair = '') {
 
 module.exports = steps;
 
+//function steps(n, row = 1, col = 1, str = '') {
+  /*--Pesudo Code: Solution (Recusive)--
+  1. make it as a matrix, and you need to build a stair from 1 to n by row
+  2. extend the function steps(n, row,col, str) that can fullfill the recursive criteria.
+    2-a. if the col > n && row = n, then finish it
+    2-b. if the col > row, then str append space ' ' and call steps(n, row+1, col, str)
+    2-c. if the index of col <= row, then append signal '#' and call steps(n, row+1, col, str)
+    2-d. if the col > n, then call steps(n, row+1, 1, '')
+  
+  
+  if(col > n && row === n) return console.log(str)
+  
+  if(col > n){
+      console.log(str)
+      return steps(n, row+1, 1, '');
+  }
+  
+  return col > row ? steps(n, row, col+1, str += ' ') : steps(n, row, col+1, str += '#');
+  */
+// }
+
+
 // function steps(n) {
-//   for (let row = 0; row < n; row++) {
-//     let stair = '';
-//
-//     for (let column = 0; column < n; column++) {
-//       if (column <= row) {
-//         stair += '#';
-//       } else {
-//         stair += ' ';
-//       }
-//     }
-//
-//     console.log(stair);
-//   }
+  /* --Pesudo Code: Solution (Nested loop)--
+  1. A loop from 1 to number:N 
+  2. loop
+    a. decide how many signal # and space ' ' by the index
+    b. make them to the different array, arrSignal and arrSpace
+    c. concat arrSignal and arrSpace, and print it
+  
+  
+  for(let numOfSignal = 1; numOfSignal<=n; numOfSignal++){
+      const numOfSpace = n - numOfSignal;
+      const arrSignal = new Array(numOfSignal).fill('#');
+      const arrSpace = new Array(numOfSpace).fill(' ');
+      console.log(arrSignal.concat(arrSpace).join(''))
+  }
+  */
 // }
